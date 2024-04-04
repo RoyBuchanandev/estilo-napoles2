4bc807d859fb4474a2cb57f84ab3701d720a7c2f
Roy <93665139+RoyBuchanandev@users.noreply.github.com>
2024-04-04 21:13:11 +0200

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import Layout from "./Layout";
+import AboutPic1 from "../assets/imgs/1.jpg";
+import AboutPic2 from "../assets/imgs/2.png";
+import AboutPic3 from "../assets/imgs/3.jpg";
+import AboutPic4 from "../assets/imgs/4.jpg";
+
+const About = () => {
+  return (
+    <>
+      <Layout
+        className={
+          "flex flex-col lg:flex-row items-center justify-between pb-24"
+        }
+      >
+        <figure id="about" className="w-full h-full lg:w-[45%] flex justify-center">
+          <div className="parent">
+            <div className="div1">
+              <img
+                className="rounded-lg shadow-xl w-full h-full object-cover"
+                src={AboutPic1}
+                alt=""
+              />
+            </div>
+            <div className="div2">
+              <img
+                className="rounded-lg shadow-xl w-full h-full object-cover"
+                src={AboutPic3}
+                alt=""
+              />
+            </div>
+            <div className="div3">
+              <img
+                className="rounded-lg shadow-xl w-full h-full object-cover"
+                src={AboutPic4}
+                alt=""
+              />
+            </div>
+            <div className="div4">
+              <img
+                className="rounded-lg shadow-xl w-full h-full object-cover"
+                src={AboutPic2}
+                alt=""
+              />
+            </div>
+          </div>
+        </figure>
+        <div className="w-full lg:w-1/2">
+          <h2 className="text-5xl lg:text-7xl text-blue bavi mb-8">Nosotros</h2>
+          <p className="font-poppins text-lg text-gray">
+            Cocina abierta, horno de barro, esperando nuestras pizzas hechas con
+            masa italiana fermentada entre 24 y 48 horas, que alojan sabores
+            argentinos, logrando una fusión con lo mejor de ambas culturas en
+            cada bocado. <br /> <br />
+            Celebramos nuestra identidad con eventos que destacan nuestra pasión
+            futbolera y hermandad argentina, honrando las tradiciones de
+            nuestros antepasados, respetando la sobremesa y jugando a las cartas
+            con un postrecito de por medio. <br /> <br />
+            Formamos un lugar donde los chicos pueden pintar, y los grandes
+            pueden pedir que escuchar, para que los artistas puedan venir a
+            dejar su arte entre nuestras paredes y compartir momentos llenos de
+            amigos y buena comida.
+          </p>
+        </div>
+      </Layout>
+      <div className=" z-50 bg-[url('./assets/bordeNegro.svg')] w-full h-6 bg-no-repeat rotate-180"></div>
+    </>
+  );
+};
+
+export default About;
---
+++ src/components/MostOrders.jsx
@@ -0,0 +1,160 @@
+import React from "react";
+import Layout from "./Layout";
+import Cuatroque from "../assets/imgs/cuatroque.png";
+import Barrilete from "../assets/imgs/barrilete_cosmico.png";
+import Mortadela from "../assets/imgs/mortadela.png";
+import Margherita from "../assets/imgs/margherita.png";
+import Napo from "../assets/imgs/napo.png";
+import Pepperoni from "../assets/imgs/peperoni.png";
+import Especial from "../assets/imgs/especial.png";
+import Fuga from "../assets/imgs/fugazzeta.png";
+import Anchoa from "../assets/imgs/anchoa.png";
+
+const MostOrders = () => {
+  return (
+    <Layout className={"h-fit"}>
+      <h2 id="menu" className="text-5xl lg:text-7xl text-blue bavi mb-8 lg:mb-16 text-center">
+        Las que más salen
+      </h2>
+
+      <section className="flex flex-wrap justify-between flex-col md:flex-row gap-8">
+        
+        <article className="card group shadow-xl overflow-hidden">
+          <div className="image">
+            <img className="drop-shadow-lg" src={Cuatroque} alt="" />
+          </div>
+          <div className="card-info text-center flex justify-center">
+            <span className="bavi text-4xl text-blue group-hover:text-white duration-300 ease-in-out">
+              Cuatroque
+            </span>
+            <p className="text-gray group-hover:text-white duration-500 ease-in-out px-6">
+              Salsa pomodoro, muzzarella, provolone, parmesano, queso azul,
+              nueces, orégano y aceite de oliva extra virgen de pepperoncino.
+            </p>
+          </div>
+        </article>
+
+        <article className="card group shadow-xl">
+          <div className="image">
+            <img className="drop-shadow-lg" src={Barrilete} alt="" />
+          </div>
+          <div className="card-info">
+            <span className="bavi text-3xl text-blue group-hover:text-white duration-300 ease-in-out">
+              Barrilete Cosmico
+            </span>
+            <p className="text-gray group-hover:text-white duration-500 ease-in-out px-6">
+              Salsa pomodoro, bocconcino, albahaca, crema de pesto, aceite de
+              oliva extra vírgen de ajo y rayadura de limón
+            </p>
+          </div>
+        </article>
+
+        <article className="card group shadow-xl">
+          <div className="image">
+            <img className="drop-shadow-lg" src={Mortadela} alt="" />
+          </div>
+          <div className="card-info">
+            <span className="bavi text-3xl text-blue group-hover:text-white duration-300 ease-in-out">
+              Mortadela
+            </span>
+            <p className="text-gray group-hover:text-white duration-500 ease-in-out px-6">
+              Salsa pomodoro, muzzarella, mortadela de pistachos, ajíes en
+              vinagre, albahaca y aceite de oliva extra vírgen de ajo.
+            </p>
+          </div>
+        </article>
+
+        {/* Aquí empiezan las nuevas tarjetas */}
+        
+        <article className="card group shadow-xl overflow-hidden">
+          <div className="image">
+            <img className="drop-shadow-lg" src={Especial} alt="" />
+          </div>
+          <div className="card-info text-center flex justify-center">
+            <span className="bavi text-4xl text-blue group-hover:text-white duration-300 ease-in-out">
+              Especial
+            </span>
+            <p className="text-gray group-hover:text-white duration-500 ease-in-out px-6">
+              Salsa pomodoro, muzzarella, jamón natural, morrones asados, aceitunas negras, orégano, oliva extra virgen de ajo
+            </p>
+          </div>
+        </article>
+
+        <article className="card group shadow-xl">
+          <div className="image">
+            <img className="drop-shadow-lg" src={Fuga} alt="" />
+          </div>
+          <div className="card-info">
+            <span className="bavi text-3xl text-blue group-hover:text-white duration-300 ease-in-out">
+              Fugazzeta
+            </span>
+            <p className="text-gray group-hover:text-white duration-500 ease-in-out px-6">
+              Muzzarella, variedad de cebollas, orégano, mix de aceitunas, oliva extra virgen al ajo
+            </p>
+          </div>
+        </article>
+
+        <article className="card group shadow-xl">
+          <div className="image">
+            <img className="drop-shadow-lg" src={Anchoa} alt="" />
+          </div>
+          <div className="card-info">
+            <span className="bavi text-3xl text-blue group-hover:text-white duration-300 ease-in-out">
+              Anchoa
+            </span>
+            <p className="text-gray group-hover:text-white duration-500 ease-in-out px-6">
+              Salsa pomodoro, muzzarella, anchoas, salsa picante
+            </p>
+          </div>
+        </article>
+
+          {/* Aquí empiezan las nuevas tarjetas */}
+        
+          <article className="card group shadow-xl overflow-hidden">
+          <div className="image">
+            <img className="drop-shadow-lg" src={Margherita} alt="" />
+          </div>
+          <div className="card-info text-center flex justify-center">
+            <span className="bavi text-4xl text-blue group-hover:text-white duration-300 ease-in-out">
+            Margherita
+            </span>
+            <p className="text-gray group-hover:text-white duration-500 ease-in-out px-6">
+            Salsa pomodoro, bocconcino, albahaca, oliva extra virgen al ajo.
+            </p>
+          </div>
+        </article>
+
+        <article className="card group shadow-xl">
+          <div className="image">
+            <img className="drop-shadow-lg" src={Napo} alt="" />
+          </div>
+          <div className="card-info">
+            <span className="bavi text-3xl text-blue group-hover:text-white duration-300 ease-in-out">
+            Napo
+            </span>
+            <p className="text-gray group-hover:text-white duration-500 ease-in-out px-6">
+            Salsa pomodoro, muzzarella, cherry, provenzal, aceitunas
+            </p>
+          </div>
+        </article>
+
+        <article className="card group shadow-xl">
+          <div className="image">
+            <img className="drop-shadow-lg" src={Pepperoni} alt="" />
+          </div>
+          <div className="card-info">
+            <span className="bavi text-3xl text-blue group-hover:text-white duration-300 ease-in-out">
+            Pepperoni
+            </span>
+            <p className="text-gray group-hover:text-white duration-500 ease-in-out px-6">
+              Salsa pomodoro, muzzarella, pepperoni, verdeo
+            </p>
+          </div>
+        </article>
+        
+      </section>
+    </Layout>
+  );
+};
+
+export default MostOrders;
